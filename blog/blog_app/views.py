d4d160dc9f6cee8a2b5224d4b560f157afcf9462
Sergey Buad <sergeybv2286@gmail.com>
2018-09-16 23:43:06 +0300

    added form view and class

--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from .forms import EmailPostForm
 from .models import Post
 
 
@@ -27,6 +28,21 @@ def post_detail(request, year, month, day, post):
                                    publish__month=month,
                                    publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+# Handles the form and sends an email
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    if request.method == 'POST':
+        # forms was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # form fields passed validation
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form})
 
 
 class PostListView(ListView):
